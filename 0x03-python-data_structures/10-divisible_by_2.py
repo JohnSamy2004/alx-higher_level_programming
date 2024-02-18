379e7f129b73c8f01728414f86869143af88ecab
JohnSamy2004 <jsamy2950@gmail.com>
2024-02-18 14:22:00 +0000

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = my_list
+    for i in new:
+        if i % 2 == 0:
+            new[i] = True
+        else:
+            new[i] = False
+
+    return new
