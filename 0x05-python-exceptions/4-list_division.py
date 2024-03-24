00052f308a606e582fcdd708a7e11aac70904574
JohnSamy2004 <jsamy2950@gmail.com>
2024-03-24 17:43:00 +0000

    103

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+st_division(my_list_1, my_list_2, list_length):
+    division_list = []
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            div = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            div = 0
+            print("division by 0")
+        except IndexError:
+            div = 0
+            print("out of range")
+        finally:
+            division_list.append(div)
+    return division_list
