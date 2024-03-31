c5faa671003d4839897eb163644b25a57f05d2fd
JohnSamy2004 <jsamy2950@gmail.com>
2024-03-31 01:41:30 +0000

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''this is Square class and there
+is inside it size->a private instance attribute'''
+
+
+class Square:
+    '''here we defined the instance of Square class'''
+
+
+    def __init__(self, size):
+        self.__size = size 
