79fc2b302dcfb6c962a2618dd37b4e5c4d412580
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-12 23:20:05 +0000

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    class square from rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+        square inerits from BaseGeometry
+        Attributes:
+            size must be private. No getter or setter
+            size must be a positive integer, validated by integer_validator
+
+        Methods:
+            __init__ - initialises the square.
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        '''area of square'''
+        return (self.__size * self.__size)
