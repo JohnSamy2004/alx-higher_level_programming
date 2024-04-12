db48b4e05355affa20cd31af76b9578c1244a3c0
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-12 21:38:20 +0000

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''class Rectangle from BaseGeomerty'''
+
+
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle:
+    '''Class is documented''' 
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
