a974b4fa15d8a8bf824fa7e086c5a208aa955e7f
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-12 18:57:03 +0000

    5 & 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''an empty class'''
+
+def BaseGeometry:
+    '''represents Base Geometry'''
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''an empty class'''
+
+def BaseGeometry:
+    '''represents Base Geometry'''
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
