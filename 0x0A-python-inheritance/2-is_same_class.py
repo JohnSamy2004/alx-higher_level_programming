75fbcab26e35d88d9a74ae5da500046fe8ea4597
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-12 18:33:27 +0000

    1

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 
 def is_same_class(obj, a_class):
     '''test the type of object'''
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
