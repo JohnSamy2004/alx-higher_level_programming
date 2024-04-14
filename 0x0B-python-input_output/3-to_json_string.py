a41de8a77f79abfce7e261a4e03e509fff0d3bb8
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-14 21:24:06 +0000

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import json
 '''convert a Python data structure to a JSON string'''
+
+
+import json
 
 
 def to_json_string(my_obj):
     '''string to json'''
-    return json.dumps(my_obj)
+    return (json.dumps(my_obj))
