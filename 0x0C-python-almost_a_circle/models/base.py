15916cbc44d1d21c0f194c6fcec807dd8bd31c69
JohnSamy2004 <jsamy2950@gmail.com>
2024-04-20 22:11:46 +0000

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''test 1'''
+
+
+class Base:
+    '''this is a base'''
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
