f53fa1683b6545632c28c91fc3bc5584580867ec
JohnSamy2004 <jsamy2950@gmail.com>
2024-07-30 17:19:26 +0300

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
-if (argv.length === 3) {
-  console.log('Argument found');
-} else if (argv.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log('Argumets found');
+  console.log('Arguments found');
 }
