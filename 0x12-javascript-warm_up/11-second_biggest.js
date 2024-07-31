777b46e43f14fd815aeb493cd774bf0442cc6cbc
JohnSamy2004 <jsamy2950@gmail.com>
2024-07-31 06:45:10 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -18,3 +18,14 @@ if (process.argv.length <= 3) {
   }
   console.log(sec);
 }
+/* or
+ * #!/usr/bin/node
+ * searches the second biggest integer in the list of arguments.
+ *
+ * if (process.argv.length <= 3) {
+ *  console.log(0);
+ *  } else {
+ * const list = process.argv.sort();
+ * console.log(list.reverse()[1]);
+ * }
+ **/
