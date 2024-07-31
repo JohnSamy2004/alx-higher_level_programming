783a8852e0b636125997c46ef744712af7f32410
JohnSamy2004 <jsamy2950@gmail.com>
2024-07-31 05:08:15 +0300

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let num = parseInt(process.argv[2], 10);
+
+function fact (num) {
+  if (isNaN(num) || num === 1 || num <= 0) {
+    return (1);
+  } else {
+    return (num * fact(num - 1));
+  }
+}
+console.log(fact(num));
