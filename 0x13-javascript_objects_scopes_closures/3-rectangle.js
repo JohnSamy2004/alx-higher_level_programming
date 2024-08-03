f4c1d899cef52197698ba5d896267990500c4c94
JohnSamy2004 <jsamy2950@gmail.com>
2024-08-04 01:09:14 +0300

    3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // leaves it empty or assign arguments with undefined
+      // this.width = undefined;
+      // this.height = undefined;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        console.log('X');
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
