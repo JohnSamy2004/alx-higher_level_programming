b424b52fda166dd6d67d65f84c36933643c911f9
JohnSamy2004 <jsamy2950@gmail.com>
2024-08-04 01:11:39 +0300

    3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -13,8 +13,8 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         console.log('X');
       }
     }
