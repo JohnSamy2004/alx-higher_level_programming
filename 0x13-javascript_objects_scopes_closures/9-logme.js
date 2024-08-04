390c72be0ce797f70815e498e895b1c0c554cea9
JohnSamy2004 <jsamy2950@gmail.com>
2024-08-04 07:04:48 +0300

    9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 exports.logMe = function (item) {
-	let count = 0;
-	this.item = item;
-
-	if (this.item) {
-		console.log(count: this.item);
-		count++;
-	}
-}
+  let count = 0;
+  console.log(`${count}: ${this.item}`);
+  count++;
+};
