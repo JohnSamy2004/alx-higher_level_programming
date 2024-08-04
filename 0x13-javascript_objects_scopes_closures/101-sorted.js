892f9beb91e31d6bc29af0f64047306d6efdafe8
JohnSamy2004 <jsamy2950@gmail.com>
2024-08-04 08:26:27 +0300

    advanced

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+!/usr/bin/node
 const dict = require('./101-data').dict;
 
 const newDict = {};
@@ -6,3 +6,4 @@ const newDict = {};
 for (const [key, val] of Object.entries(dict)) {
   newDict[val] ? newDict[val].push(key) : (newDict[val] = [key]);
 }
+console.log(newDict);
